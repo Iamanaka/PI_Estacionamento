cc5df6a4a42418a8860e4df8e71f8f801dc1880d
Lauro Iamanaka <lauroiamanaka@gmail.com>
2025-04-14 19:29:13 -0300

    Iniciando o Projeto Integrador.

---
+++ PI/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PI
+{
+    partial class FormGERAL
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUSUARIO = new System.Windows.Forms.Label();
+            this.labelSENHA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelUSUARIO
+            // 
+            this.labelUSUARIO.AutoSize = true;
+            this.labelUSUARIO.Location = new System.Drawing.Point(108, 118);
+            this.labelUSUARIO.Name = "labelUSUARIO";
+            this.labelUSUARIO.Size = new System.Drawing.Size(43, 13);
+            this.labelUSUARIO.TabIndex = 0;
+            this.labelUSUARIO.Text = "Usuario";
+            // 
+            // labelSENHA
+            // 
+            this.labelSENHA.AutoSize = true;
+            this.labelSENHA.Location = new System.Drawing.Point(108, 163);
+            this.labelSENHA.Name = "labelSENHA";
+            this.labelSENHA.Size = new System.Drawing.Size(38, 13);
+            this.labelSENHA.TabIndex = 1;
+            this.labelSENHA.Text = "Senha";
+            // 
+            // FormGERAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelSENHA);
+            this.Controls.Add(this.labelUSUARIO);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormGERAL";
+            this.Text = "Welcome!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUSUARIO;
+        private System.Windows.Forms.Label labelSENHA;
+    }
+}
+
